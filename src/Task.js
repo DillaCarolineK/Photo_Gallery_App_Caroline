6fed788ba91234fb4445aa53e87046f66268f932
Dilla Caroline Khairunnisa <dillajepara578@students.unnes.ac.id>
2022-12-19 10:49:19 +0700

    Update Task.js

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://gallery-app-caroline.netlify.app" // TODO: replace this
 const studentName = "Dilla Caroline Khairunnisa'" // TODO: replace this
 const studentId = "FE2795717" // TODO: replace this
 
